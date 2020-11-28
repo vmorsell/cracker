91b5b3c44b999db58030579903d4a1bfe5ae6fab
Viktor Mörsell <viktor.morsell@northvolt.com>
2020-11-28 20:38:45 +0100

    Add dictionary attack support

---
+++ dictionary/crack.go
@@ -0,0 +1,47 @@
+package dictionary
+
+import (
+	"bytes"
+	"time"
+
+	"github.com/vmorsell/cracker-poc/cipherlib"
+)
+
+type Strategy struct {
+	Cipher cipherlib.Interface
+}
+
+type Result struct {
+	Ok       bool
+	Hash     []byte
+	Password []byte
+	Tries    int
+	Time     time.Duration
+}
+
+// Crack tries to find the original password using the loaded dictionary
+func (d *Dictionary) Crack(hash []byte, s *Strategy) *Result {
+	start := time.Now()
+
+	for i, w := range d.Words {
+		h := s.Cipher.Hash(w)
+		if bytes.Equal(hash, h) {
+			time := time.Now().Sub(start)
+			return &Result{
+				Ok:       true,
+				Hash:     h,
+				Password: w,
+				Tries:    i + 1,
+				Time:     time,
+			}
+		}
+	}
+
+	// No match
+	time := time.Now().Sub(start)
+	return &Result{
+		Ok:    false,
+		Tries: len(d.Words),
+		Time:  time,
+	}
+}
---
+++ dictionary/dictionary.go
@@ -0,0 +1,51 @@
+package dictionary
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+)
+
+type Interface interface {
+	Crack(hash []byte, s *Strategy) *Result
+}
+
+type Dictionary struct {
+	Words [][]byte
+}
+
+var _ Interface = &Dictionary{}
+
+func New(src string) (*Dictionary, error) {
+	if src == "" {
+		return nil, errors.New("missing src")
+	}
+
+	file, err := os.Open(src)
+	if err != nil {
+		return nil, fmt.Errorf("open: %w", err)
+	}
+	defer file.Close()
+
+	reader := bufio.NewReader(file)
+
+	var words [][]byte
+	for {
+		line, _, err := reader.ReadLine()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return nil, fmt.Errorf("readLine: %w", err)
+		}
+
+		w := make([]byte, len(line))
+		copy(w, line)
+		words = append(words, w)
+	}
+	return &Dictionary{
+		Words: words,
+	}, nil
+}
