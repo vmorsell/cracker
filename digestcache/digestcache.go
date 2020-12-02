b37fa8c9849e3a0b563a2db765c6563252f95228
Viktor Mörsell <viktor.morsell@northvolt.com>
2020-12-02 22:59:06 +0100

    Add cache package

---
+++ digestcache/digestcache.go
@@ -0,0 +1,35 @@
+package digestcache
+
+// Interface defines the public API exposed by this package
+type Interface interface {
+	Add(hash []byte, text []byte)
+	Lookup(hash []byte) []byte
+}
+
+// DigestCache is the main struct
+type DigestCache struct {
+	Records map[string][]byte
+}
+
+var _ Interface = &DigestCache{}
+
+// New returns a pointer to a DigestCache struct
+func New() *DigestCache {
+	r := make(map[string][]byte)
+	return &DigestCache{
+		Records: r,
+	}
+}
+
+// Add inserts a record to the cache
+func (dc *DigestCache) Add(hash []byte, text []byte) {
+	dc.Records[string(hash)] = text
+}
+
+// Lookup finds and returns a record from the cache
+func (dc *DigestCache) Lookup(hash []byte) []byte {
+	if r, ok := dc.Records[string(hash)]; ok {
+		return r
+	}
+	return nil
+}
