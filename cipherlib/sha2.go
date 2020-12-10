9cd886959eb22cb3a42ae279c998c01f57d16c51
Viktor Mörsell <viktor.morsell@northvolt.com>
2020-12-10 07:02:42 +0100

    Use SHA instead of Sha

--- cipherlib/sha2.go
+++ cipherlib/sha2.go
@@ -6,24 +6,24 @@ import (
 	"fmt"
 )
 
-// Sha2 represents the SHA2 cipher method.
+// SHA2 represents the SHA2 cipher method.
 //
-// Note that the zero value for Sha2 is not a valid configuration. Create a
-// Sha2 struct using New().
-type Sha2 struct {
+// Note that the zero value for SHA2 is not a valid configuration. Create a
+// SHA2 struct using New().
+type SHA2 struct {
 	Bits int
 }
 
-var _ Interface = &Sha2{}
+var _ Interface = &SHA2{}
 
 // Bitsizes defines the valid SHA2 bitsizes.
 var Bitsizes = []int{224, 256, 384, 512}
 
-// NewSha2 creates a *Sha2 struct from a cipher bitsize.
-func NewSha2(bits int) (*Sha2, error) {
+// NewSHA2 creates a *SHA2 struct from a cipher bitsize.
+func NewSHA2(bits int) (*SHA2, error) {
 	for _, v := range Bitsizes {
 		if bits == v {
-			return &Sha2{
+			return &SHA2{
 				Bits: v,
 			}, nil
 		}
@@ -32,36 +32,36 @@ func NewSha2(bits int) (*Sha2, error) {
 }
 
 // Hash calculates the SHA2 digest.
-func (s *Sha2) Hash(in []byte) []byte {
+func (s *SHA2) Hash(in []byte) []byte {
 	switch s.Bits {
 	case 224:
-		return execSha224(in)
+		return execSHA224(in)
 	case 384:
-		return execSha384(in)
+		return execSHA384(in)
 	case 512:
-		return execSha512(in)
+		return execSHA512(in)
 	case 256:
-		return execSha256(in)
+		return execSHA256(in)
 	}
 	return nil
 }
 
-func execSha224(in []byte) []byte {
+func execSHA224(in []byte) []byte {
 	h := sha256.Sum224(in)
 	return h[:]
 }
 
-func execSha256(in []byte) []byte {
+func execSHA256(in []byte) []byte {
 	h := sha256.Sum256(in)
 	return h[:]
 }
 
-func execSha384(in []byte) []byte {
+func execSHA384(in []byte) []byte {
 	h := sha512.Sum384(in)
 	return h[:]
 }
 
-func execSha512(in []byte) []byte {
+func execSHA512(in []byte) []byte {
 	h := sha512.Sum512(in)
 	return h[:]
 }
